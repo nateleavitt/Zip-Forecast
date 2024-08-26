c9379cbbe66873ac3bbcb7494fa6c7afce19e479
Nathan Leavitt <nateleavitt@gmail.com>
2024-08-25 18:27:32 -0700

    Adding attributes and validations

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,2 +1,10 @@
 class Forecast < ApplicationRecord
+  include ActiveModel::Model
+
+  # Attributes
+  attr_accessor :street, :city, :state, :zip, presence: true
+
+  # Validations
+  validates :street, :city, :state, :zip, presence: true
+  validates :zip, format: { with: /\A\d{5}(-\d{4})?\z/, message: "should be in the format 12345 or 12345-6789" }
 end
