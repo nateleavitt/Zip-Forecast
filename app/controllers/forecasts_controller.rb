f4506f7a3f3d93a7b37a1e50281e162f872c0298
Nathan Leavitt <nateleavitt@gmail.com>
2024-08-25 20:40:04 -0700

    Adding initial controller actions

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,2 +1,25 @@
 class ForecastsController < ApplicationController
+  def index
+    @forecasts = []
+  end
+
+  def create
+    @forecast = Forecast.new(forecast_params)
+
+    respond_to do |format|
+      if @forecast.valid?
+        format.turbo_stream
+        format.html { redirect_to forecast_path(zip_code: @forecast.zip_code), notice: "Forecast was successfully fetched." }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@forecast)}_form", partial: "form", locals: { forecast: @forecast }) }
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def forecast_params
+    params.require(:forecast).permit(:street, :city, :state, :zip_code)
+  end
 end
