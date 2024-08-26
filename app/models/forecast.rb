a818673b70ca26403aaa0136cdc84ae9d9f98215
Nathan Leavitt <nateleavitt@gmail.com>
2024-08-25 18:42:41 -0700

    Adding some validation and updating zip to zip_code

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -2,9 +2,10 @@ class Forecast < ApplicationRecord
   include ActiveModel::Model
 
   # Attributes
-  attr_accessor :street, :city, :state, :zip, presence: true
+  attr_accessor :street, :city, :state, :zip_code, presence: true
 
   # Validations
-  validates :street, :city, :state, :zip, presence: true
-  validates :zip, format: { with: /\A\d{5}(-\d{4})?\z/, message: "should be in the format 12345 or 12345-6789" }
+  validates :street, :city, :state, :zip_code, presence: true
+  validates :zip_code, format: { with: /\A\d{5}(-\d{4})?\z/,
+    message: "should be in the format 12345 or 12345-6789" }
 end
