1649b8456d5e408222ecfabb4da9d52b797e35e2
Nathan Leavitt <nateleavitt@gmail.com>
2024-08-26 09:49:18 -0700

    Adding safe navigation

--- app/services/weather_manager/weather_parser.rb
+++ app/services/weather_manager/weather_parser.rb
@@ -1,11 +1,14 @@
 module WeatherManager
   class WeatherParser < ApplicationService
+    FORECAST_DAYS = 4
+
     attr_reader :response
 
     def initialize(response)
       @response = response
     end
 
+    # Main method to call and return parsed weather data
     def call
       {
         current: parse_current,
@@ -13,27 +16,33 @@ module WeatherManager
       }
     end
 
+    private
+
+    # Parses the current weather data from the API response
     def parse_current
       {
-        location: @response["location"]["name"] + ", " + @response["location"]["region"],
-        temperature: @response["current"]["temp_f"],
-        condition: @response["current"]["condition"]["text"],
-        icon: @response["current"]["condition"]["icon"],
-        last_updated: @response["current"]["last_updated"]
+        location: "#{@response.dig('location', 'name')}, #{@response.dig('location', 'region')}",
+        temperature: @response.dig("current", "temp_f"),
+        condition: @response.dig("current", "condition", "text"),
+        icon: @response.dig("current", "condition", "icon"),
+        last_updated: @response.dig("current", "last_updated")
       }
     end
 
+    # Parses the forecast data for up to 3 days from the API response
     def parse_forecast
-      forecast = @response["forecast"]["forecastday"].map do |day|
+      return [] unless @response.dig("forecast", "forecastday") # Return an empty array if no forecast
+
+      forecast = @response.dig("forecast", "forecastday")&.map do |day|
         {
-          max_temp: day["day"]["maxtemp_f"],
-          min_temp: day["day"]["mintemp_f"],
-          condition: day["day"]["condition"]["text"],
-          icon: day["day"]["condition"]["icon"],
+          max_temp: day.dig("day", "maxtemp_f"),
+          min_temp: day.dig("day", "mintemp_f"),
+          condition: day.dig("day", "condition", "text"),
+          icon: day.dig("day", "condition", "icon"),
           date: day["date"]
         }
       end
-      forecast[0..4] # Return today and next 3 days
+      forecast[0..FORECAST_DAYS-1] if forecast # Safeguard if forecast is nil
     end
   end
 end
